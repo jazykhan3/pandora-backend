81d41eff030c3b8681b8d4abd32b2ca722ed8b7c
JahanZaib Sultan <80755940+jazykhan3@users.noreply.github.com>
2025-08-11 13:21:34 +0500

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -51,7 +51,7 @@ app.use((err: any, req: express.Request, res: express.Response, next: express.Ne
 });
 
 // 404 handler
-app.use('*', (req, res) => {
+app.use((req, res) => {
   res.status(404).json({ error: 'Endpoint not found' });
 });
 
